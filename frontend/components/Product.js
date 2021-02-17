a0e8918bb901c94633ba336cdf617b5907709d32
SC Graphics <scgraphics@SCs-iMac.local>
2021-02-17 17:26:53 -0500

    hit speedbump

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -16,7 +16,18 @@ export default function Product({ product }) {
       </Title>
       <PriceTag>{formatMoney(product.price)}</PriceTag>
       <p>{product.description}</p>
-      {/* TODO: ADD BUTTONS TO EDIT AND DELETE item */}
+      <div className="buttonList">
+        <Link
+          href={{
+            pathname: 'update',
+            query: {
+              id: product.id,
+            },
+          }}
+        >
+          Edit
+        </Link>
+      </div>
     </ItemStyles>
   );
 }
